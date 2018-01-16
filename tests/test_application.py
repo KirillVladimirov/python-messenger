ec81e3096d1208da36e68ac65a3d0d1b516c9547
Kirill Vladimirov <kvladimirov.main@gmail.com>
2018-01-16 14:06:36 +0300

    add env configs

---
+++ tests/test_application.py
@@ -0,0 +1,31 @@
+# coding=utf-8
+
+import unittest
+
+
+class TestApplication(unittest.TestSuite):
+
+    def test_create_app(self):
+        pass
+
+    def test_available_requirements(self):
+        pass
+
+    def test_make_config(self):
+        pass
+
+    def test_set_def_env(self):
+        pass
+
+    def test_set_prod_env(self):
+        pass
+
+    def test_set_test_env(self):
+        pass
+
+    def test_logging(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
