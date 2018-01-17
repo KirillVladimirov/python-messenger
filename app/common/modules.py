e1e2335af232acceb6675922350dbfc8a43fd18a
Kirill Vladimirov <kvladimirov.main@gmail.com>
2018-01-17 18:32:41 +0300

    db WIP

--- app/common/modules.py
+++ app/common/modules.py
@@ -1,12 +1,12 @@
 # coding=utf-8
 
-# Import the database object (db) from the main application module
-# We will define this inside /app/__init__.py in the next sections.
 from app import db
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, DateTime, Integer, String, ForeignKey
 
 
 # Define a base model for other database tables to inherit
-class Base(db.Model):
+class Base(db.Base):
     __abstract__ = True
 
     id = db.Column(db.Integer, primary_key=True)
@@ -39,16 +39,6 @@ class User(Base):
 
 
 
-from sqlalchemy import (
-    Column,
-    DateTime,
-    Integer,
-    String,
-    ForeignKey
-)
-
-from sqlalchemy.orm import relationship
-from app.common import Base
 
 
 class User(Base):
