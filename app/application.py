2c1bcb82ea3938c2b86e69785db37793646593eb
Kirill Vladimirov <kvladimirov.main@gmail.com>
2018-01-18 16:20:16 +0300

    fix todolist

--- app/application.py
+++ app/application.py
@@ -51,27 +51,6 @@ class Application(object):
         logger.addHandler(ch)
         return logger
 
-    # def name(self):
-    #     """The name of the application.  This is usually the import name
-    #     with the difference that it's guessed from the run file if the
-    #     import name is main.  This name is used as a display name when
-    #     Flask needs the name of the application.  It can be set and overridden
-    #     to change the value.
-    #     .. versionadded:: 0.8
-    #     """
-    #     if self.import_name == '__main__':
-    #         fn = getattr(sys.modules['__main__'], '__file__', None)
-    #         if fn is None:
-    #             return '__main__'
-    #         return os.path.splitext(os.path.basename(fn))[0]
-    #     return self.import_name
-    #
-    # def __repr__(self):
-    #     return '<%s %r>'.format() % (
-    #         self.__class__.__name__,
-    #         self.name,
-    #     )
-
 
 class Config(dict):
 
